f676ac901eb41e2b43d40057a54073da7041918c
polyanav <161487107+polyanav@users.noreply.github.com>
2024-10-11 21:44:01 -0300

    ok

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'bolsodivo',
+    'pq devemos adorar o bolsonaro?',
+    'pq com sua arma de 22cm ele tras paz e prazer para todos nos seu poder é infinito'
+)
+
+criaCartao(
+    'goza em mim bolsonaro',
+    'um exemplo de poder dele',
+    'Apenas com 0% do poder dele pode acabar com a fome da venezuela'
+)
+
+criaCartao(
+    'amor eterno',
+    'devemos temer ao bolsonaro?',
+    'não apenas se vc seja feminista ou lgbt petista caso ao contrario bolsonaro te mostrara todo seu amor'
+)
+
+criaCartao(
+    'fim',
+    'oque significa bolsonaro?',
+    'amor gratidao e felicidade e bondade'
+)
