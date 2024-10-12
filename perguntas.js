f017f24905423b82191fbb93b7c4a40c241e16a5
polyanav <161487107+polyanav@users.noreply.github.com>
2024-10-11 21:52:34 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'bolsodivo',
-    'pq devemos adorar o bolsonaro?',
-    'pq com sua arma de 22cm ele tras paz e prazer para todos nos seu poder é infinito'
+    'tristeza',
+    'porque deveremos entra no jogo do discontente?',
+    'para espalhar a depressao e tudo que ha de ruim em nossos coraçoes'
 )
 
 criaCartao(
-    'goza em mim bolsonaro',
-    'um exemplo de poder dele',
-    'Apenas com 0% do poder dele pode acabar com a fome da venezuela'
+    'discontente',
+    'oque é o jogo do discontente?',
+    'pra romantizar a nossa depressao'
 )
 
 criaCartao(
-    'amor eterno',
-    'devemos temer ao bolsonaro?',
-    'não apenas se vc seja feminista ou lgbt petista caso ao contrario bolsonaro te mostrara todo seu amor'
+    'ansiedade',
+    'pq devemos ser contra o jogo do contente?',
+    'pq pessoas felizes sao um saco de lidar'
 )
 
 criaCartao(
-    'fim',
-    'oque significa bolsonaro?',
-    'amor gratidao e felicidade e bondade'
+    'timidez',
+    'oq significa ser descontente?',
+    'ser triste a vida toda e nao se culpar por isso'
 )
